0b1e0e59f0a6d451d7b13405c1da454baf232a22
PedroLor1a <pedroloria003@gmail.com>
2023-10-20 00:50:14 -0300

    terminada?

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { conn } = require("./src/db");
 // ! Despues de terminar la db pasar a alter: true
 conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
-    console.log("funcionando en el http://localhost:3001");
+    console.log("http://localhost:3001");
   });
 });
